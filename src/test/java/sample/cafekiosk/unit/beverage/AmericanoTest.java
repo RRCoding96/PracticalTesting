daf793b652a668b12d66532860cdcc13b22da911
김윤욱 <reznov03048@gmail.com>
2024-08-08 20:30:35 +0900

    JUnit5로 테스트하기

---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,30 @@
+package sample.cafekiosk.unit.beverage;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AmericanoTest {
+
+    @DisplayName("")
+    @Test
+    void getName() {
+        // given
+        Americano americano = new Americano();
+
+        // when
+        // then
+//        assertEquals(americano.getName(), "아메리카노");
+        assertThat(americano.getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void getPrice() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getPrice()).isEqualTo(4000);
+    }
+
+}
